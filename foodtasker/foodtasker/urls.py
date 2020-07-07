b0bf6eb5b5b8b40323bcd70819a9e2d167b9bb7d
Bernard <bernardy_ho@hotmail.com>
2020-07-07 19:55:35 +0800

    Registration Page
    * installed pillow
    * created the Restaurant model
    * Sign Up Page

--- foodtasker/foodtasker/urls.py
+++ foodtasker/foodtasker/urls.py
@@ -40,9 +40,12 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('restaurant/sign-in/', auth_views.LoginView.as_view(template_name='restaurant/sign_in.html'), name = 'restaurant-sign-in'),
-    path('restaurant/sign-out/', auth_views.LogoutView.as_view(next_page='/'), name = 'restaurant-sign-out'),
+    path('restaurant/sign-in/', auth_views.LoginView.as_view(
+        template_name='restaurant/sign_in.html'), name='restaurant-sign-in'),
+    path('restaurant/sign-out/', auth_views.LogoutView.as_view(next_page='/'),
+         name='restaurant-sign-out'),
     path('restaurant/', views.restaurant_home, name='restaurant-home'),
-    # path('restaurant/sign-up/', views.restaurant_sign_up, name = 'restaurant-sign-up'),
-] 
+    path('restaurant/sign-up/', views.restaurant_sign_up,
+         name='restaurant-sign-up'),
+]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
