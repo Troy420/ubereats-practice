5b09fdd9075889317f2937f3724ada3f2b2fd9ad
Bernard <bernardy_ho@hotmail.com>
2020-07-08 15:01:41 +0800

    Registration Function

--- foodtasker/foodtasker/urls.py
+++ foodtasker/foodtasker/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from foodtaskerapp import views
 from django.contrib.auth import views as auth_views
 
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 # ----------------------------------------------------------------------------------
 # TypeError at /restaurant/sign-in/
@@ -47,5 +47,4 @@ urlpatterns = [
     path('restaurant/', views.restaurant_home, name='restaurant-home'),
     path('restaurant/sign-up/', views.restaurant_sign_up,
          name='restaurant-sign-up'),
-]
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
