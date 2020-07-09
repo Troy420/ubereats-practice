5525f5725c46c32346273a368794a40fadfe9216
Bernard <bernardy_ho@hotmail.com>
2020-07-09 20:57:40 +0800

    Added Bootstrap CSS

--- foodtasker/foodtasker/urls.py
+++ foodtasker/foodtasker/urls.py
@@ -49,6 +49,14 @@ urlpatterns = [
     path('restaurant/', views.restaurant_home, name='restaurant-home'),
     path('restaurant/sign-up/', views.restaurant_sign_up,
          name='restaurant-sign-up'),
+    path('restaurant/account/', views.restaurant_account,
+         name='restaurant-account'),
+    path('restaurant/meal/', views.restaurant_meal,
+         name='restaurant-meal'),
+    path('restaurant/order/', views.restaurant_order,
+         name='restaurant-order'),
+    path('restaurant/report/', views.restaurant_report,
+         name='restaurant-report'),
 
     #  Sign in / Sign up / Sign out
     path('api/social/', include('rest_framework_social_oauth2.urls')),
--- foodtasker/foodtaskerapp/social_auth_pipeline.py
+++ foodtasker/foodtaskerapp/social_auth_pipeline.py
@@ -3,7 +3,6 @@
 # --------------------------------------------------------------------------------------------------------
 # from foodtaskerapp.models import Customer, Driver
 
-
 # def create_user_by_type(backend, user, request, response, *args, **kwargs):
 #     if backend.name == 'facebook':
 #         avatar = 'https://graph.facebook.com/%s/picture?type=large' % response['id']
@@ -16,12 +15,13 @@
 
 from foodtaskerapp.models import Customer, Driver
 
+
 def create_user_by_type(backend, user, response, *args, **kwargs):
     request = backend.strategy.request_data()
     if backend.name == 'facebook':
         avatar = 'https://graph.facebook.com/%s/picture?type=large' % response['id']
 
     if request["user_type"] == "driver" and not Driver.objects.filter(user_id=user.id):
-        Driver.objects.create(user_id=user.id, avatar = avatar)
+        Driver.objects.create(user_id=user.id, avatar=avatar)
     elif not Customer.objects.filter(user_id=user.id):
-        Customer.objects.create(user_id=user.id, avatar = avatar)
+        Customer.objects.create(user_id=user.id, avatar=avatar)
